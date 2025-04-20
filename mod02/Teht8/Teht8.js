5385be5d261ecf15b550a8f80e94f2a41bf729da
talvim104 <Tarya.Talvinen@metropolia.fi>
2025-04-20 17:45:10 +0300

    changes

--- mod02/Teht8/Teht8.js
+++ mod02/Teht8/Teht8.js
@@ -1,2 +1,15 @@
 'use strict'
 
+function concat(arr) {
+  let result = '';
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  return result;
+
+}
+const names = ['Johnny', 'DeeDee', 'Joey', 'Marky'];
+
+const names2 = concat(names);
+
+document.write(`<p>${names2}</p>`)
