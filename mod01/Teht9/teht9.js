ecbfed3d1ed5288c50949a8cc31136c239bcfa03
talvim104 <Tarya.Talvinen@metropolia.fi>
2025-04-21 17:17:23 +0300

    changes

--- mod01/Teht9/teht9.js
+++ mod01/Teht9/teht9.js
@@ -1,9 +1,23 @@
 'use strict';
 
 const number = parseInt(prompt("Enter number:"));
-if (number < 2) {
 
-}
+let primeNum = true;
+if (number <= 1) {
+  primeNum = false
+} else {
+  for ( let i = 2; i < number; i++)
+    if (number % i === 0) {
+      primeNum = false;
+      break;
+    }
+  }
+
+if (primeNum) {
+  document.write(number + " is a prime number.");
+} else {
+    document.write(number + " is not a prime number.");
+  }
 
 
 
