a5f6276aeafafa0520ca7d75dfe5a4a8bbe7f7c8
talvim104 <Tarya.Talvinen@metropolia.fi>
2025-04-22 19:55:46 +0300

    changes

--- mod03/t2/2.js
+++ mod03/t2/2.js
@@ -1,4 +1,16 @@
 'use strict';
 
+const target = document.getElementById('target');
 
+const elem1 = document.createElement('li');
+elem1.textContent = 'First item';
+target.appendChild(elem1);
 
+const elem2 = document.createElement('li');
+elem2.textContent = 'Second item';
+elem2.classList.add('my-item');
+target.appendChild(elem2);
+
+const elem3 = document.createElement('li');
+elem3.textContent = 'Third item';
+target.appendChild(elem3);
